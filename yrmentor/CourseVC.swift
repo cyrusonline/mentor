665ff2b44f8d5c03a3b6fe7f165e7c6095e59344
cyrusonline <online_mad@yahoo.com.hk>
2017-06-03 10:15:37 +0800

    add table view of coures

---
+++ yrmentor/CourseVC.swift
@@ -0,0 +1,35 @@
+//
+//  CourseVC.swift
+//  yrmentor
+//
+//  Created by Cyrus Chan on 31/5/2017.
+//  Copyright © 2017 ckmobile.com. All rights reserved.
+//
+
+import UIKit
+
+class CourseVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
